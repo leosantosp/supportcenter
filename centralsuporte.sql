6cf8c1b161fc9e3f3d2541f55df0855d6f59ca77
Leonardo <leo.santosp@outlook.com>
2022-11-06 15:12:01 -0300

    Atualizar o nome de alguns arquivos, move-los e deletar alguns

--- centralsuporte.sql
+++ centralsuporte.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 01-Nov-2022 às 21:48
--- Versão do servidor: 10.4.18-MariaDB
--- versão do PHP: 8.0.3
+-- Tempo de geração: 06-Nov-2022 às 19:07
+-- Versão do servidor: 10.4.17-MariaDB
+-- versão do PHP: 8.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `catalogoenderecos`
+--
+
+CREATE TABLE `catalogoenderecos` (
+  `id` int(11) NOT NULL,
+  `tratamento` varchar(255) NOT NULL,
+  `empresa` varchar(255) NOT NULL,
+  `cargo` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `catalogoenderecos`
+--
+
+INSERT INTO `catalogoenderecos` (`id`, `tratamento`, `empresa`, `cargo`, `email`, `senha`) VALUES
+(2, 'GRU TI | Leonardo Santos', 'Assistente de TI', 'VIPEX', 'leonardo.santos@vipextransportes.com.br', '1234456789');
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `colaboradores`
 --
 
@@ -33,7 +55,7 @@ CREATE TABLE `colaboradores` (
   `department` varchar(20) CHARACTER SET utf8 NOT NULL,
   `email` varchar(100) CHARACTER SET utf8 NOT NULL,
   `phone` int(15) NOT NULL,
-  `unity` varchar(10) CHARACTER SET utf8 NOT NULL,
+  `company` varchar(10) CHARACTER SET utf8 NOT NULL,
   `birth` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -41,15 +63,95 @@ CREATE TABLE `colaboradores` (
 -- Extraindo dados da tabela `colaboradores`
 --
 
-INSERT INTO `colaboradores` (`id`, `fullname`, `department`, `email`, `phone`, `unity`, `birth`) VALUES
+INSERT INTO `colaboradores` (`id`, `fullname`, `department`, `email`, `phone`, `company`, `birth`) VALUES
 (7, 'Pietro Cativelli', 'TI', 'pietro.cativelli@vipextransportes.com.br', 1222, '11 - GRU', '2004-01-20'),
 (8, 'Leonardo Santos', 'TI', 'leonardo.santos@vipextransportes.com.br', 1209, '11 - GRU', '1999-02-19'),
 (9, 'Francisco Alves', 'TI', 'francisco.alves@vipextransportes.com.br', 1166, '11 - GRU', '1984-03-14'),
 (10, 'Sidnei Oliveira', 'TI', 'sidnei.oliveira@vipextransportes.com.br', 1165, '11 - GRU', '1973-05-08');
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `companys`
+--
+
+CREATE TABLE `companys` (
+  `id` int(11) NOT NULL,
+  `compnumber` int(11) NOT NULL,
+  `cnpj` varchar(20) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `gestor` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `meetings`
+--
+
+CREATE TABLE `meetings` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `guests` varchar(500) DEFAULT NULL,
+  `link` varchar(255) DEFAULT NULL,
+  `room` varchar(100) NOT NULL,
+  `start` datetime NOT NULL,
+  `end` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `ouvidoria`
+--
+
+CREATE TABLE `ouvidoria` (
+  `id` int(11) NOT NULL,
+  `filial` varchar(3) NOT NULL,
+  `department` varchar(25) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `ramal` int(15) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `setor` varchar(255) NOT NULL,
+  `type` varchar(15) NOT NULL,
+  `report` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `profile` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `username`, `login`, `password`, `email`, `profile`) VALUES
+(1, 'Leonardo Santos', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'leonardo.santos@vipextransportes.com.br', 'admin'),
+(3, 'Francisco Alves', 'francisco.alves', 'c9d0f88705406a124fe0e2f3dd63445d', 'francisco.alves@vipextransportes.com.br', 'admin');
+
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `catalogoenderecos`
+--
+ALTER TABLE `catalogoenderecos`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices para tabela `colaboradores`
@@ -58,14 +160,68 @@ ALTER TABLE `colaboradores`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices para tabela `companys`
+--
+ALTER TABLE `companys`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `meetings`
+--
+ALTER TABLE `meetings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `ouvidoria`
+--
+ALTER TABLE `ouvidoria`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `catalogoenderecos`
+--
+ALTER TABLE `catalogoenderecos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `colaboradores`
 --
 ALTER TABLE `colaboradores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de tabela `companys`
+--
+ALTER TABLE `companys`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `meetings`
+--
+ALTER TABLE `meetings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `ouvidoria`
+--
+ALTER TABLE `ouvidoria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- index.php
+++ index.php
@@ -6,14 +6,14 @@
     session_unset();
 
 
-    include_once 'php_action/db_connect.php';
+    include_once 'admin/php_action/db_connect.php';
 
     include_once __DIR__.'/includes/header.php';
 ?>
 
     <div class="row no-gutters mt-4">
         <div class="col-12 col-md-8 offset-md-2">
-            <h3 class="text-center">CENTRAL DE SUPORTE | Cadastro de Colaboradores</h3>
+            <h3 class="text-center">CENTRAL DE SUPORTE | Lista de Contatos Internos</h3>
             <table class="table">
                 <thead class="thead-dark">
                     <tr>
@@ -22,8 +22,6 @@
                         <th>Nome</th>
                         <th>E-mail</th>
                         <th>Ramal</th>
-                        <th>Aniversário</th>
-                        <th class="text-center" colspan="2">Ações</th>
                     </tr>
                 </thead>
 
@@ -45,32 +43,9 @@
                         <td><?php echo $dados['fullname']; ?></td>
                         <td><?php echo $dados['email']; ?></td>
                         <td><?php echo $dados['phone']; ?></td>
-                        <td><?php echo date('d/m', strtotime($dados['birth'])) ?></td>
-                        <td><a href="editar.php?id=<?php echo $dados['id']; ?>" class="btn btn-primary">Editar</a></td>
-                        <td><a data-bs-toggle="modal" data-bs-target="#deleteModal<?php echo $dados['id']; ?>" class="btn btn-danger">Excluir</a></td>
                     </tr>
 
-                    <!-- Modal -->
-                    <div class="modal fade" id="deleteModal<?php echo $dados['id']; ?>" tabindex="-1" aria-labelledby="deleteModalLabel<?php echo $dados['id']; ?>" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="deleteModalLabel<?php echo $dados['id']; ?>">OPA!</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <p>Tem certeza que deseja excluir o colaborador <strong><?php echo $dados['fullname'] ?></strong> </p>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                            <form action="php_action/delete.php" method="POST">
-                                <input type="hidden" name="id" value="<?php echo $dados['id'] ?>">
-                                <button type="submit" name="btn-delete" class="btn btn-danger">Sim, quero excluir</button>
-                            </form>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
+                    
 
                     <?php 
                         endwhile; 
@@ -86,8 +61,7 @@
                 </tbody>
             </table>
 
-            <br>
-            <a href="adicionar.php" class="btn btn-success">Adicionar colaborador</a>
+            <a href="colab-excel.php">Baixar catálogo de endereços</a>
         </div>
     </div>
 
